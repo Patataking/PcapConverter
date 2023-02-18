54ebc2472a846353a1a82aa49e019867cf3bd040
Malte Jensen <m.jensen.jork@gmail.com>
2023-02-18 15:35:43 +0100

    Add comments and fix delta calculation

--- PcapConverter/Program.cs
+++ PcapConverter/Program.cs
@@ -1,15 +1,21 @@
-﻿using System.IO;
-using System.Runtime.InteropServices;
-using static System.Net.Mime.MediaTypeNames;
-
+﻿
 namespace PcapConverter
-{
+{    
     internal class Program
     {
+        // Amount of erroneous .pcap files
         static int errors = 0;
+        // Flag whether the unpatched version or patched version is used. Needed to filter the packages correctly.
         static bool isUnpatched = true;
-        static readonly string workingDirectory = Environment.CurrentDirectory;
-        static async Task Main(string[] args)
+
+        /// <summary>
+        /// This program calculates time deltas from .csv files containing info from .pcap files
+        /// The .csv files are created using Tshark
+        /// </summary>
+        /// <param name="_"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        static async Task Main(string[] _)
         {
             Console.WriteLine("Enter input path");
             var inputPath = @"" + Console.ReadLine();
@@ -22,60 +28,58 @@ namespace PcapConverter
             Console.WriteLine("Enter output path");
             var outputPath = @"" + Console.ReadLine();
 
-            Console.WriteLine("Enter p for patched Version");
-            var inp = @"" + Console.ReadLine();
-            if (inp.Equals("p"))
-            {
-                isUnpatched = false;
-            }
-
             if (!Directory.Exists(outputPath))
             {
                 throw new Exception("Path doesn't exist:\t" + outputPath);
             }
 
+            // Check input version
+            Console.WriteLine("Enter p for patched Version");
+            var inp = @"" + Console.ReadLine();
+            if (inp.Equals("p"))
+            {
+                isUnpatched = false;
+            }            
+
             // Print data directory
             Console.WriteLine("Input directory:\t" + inputPath);
             // Print data directory
             Console.WriteLine("Output directory:\t" + outputPath);
+            // Print used version
+            Console.WriteLine(isUnpatched ? "Unpatched version" : "Patched version");
 
-            //CreateCsvFiles(inputPath);
-
-
-            var deltas = new List<string>();
-
+            // Get all subdirectories of input directory
             var directories = Directory.GetDirectories(inputPath).ToList();
 
+            var deltas = new List<string>();
             var tasks = new List<Task<List<string>>>();
 
-            /*directories.ForEach(directoryPath =>
-            {
-                var outputFilePath = outputPath + directoryPath.Split('\\').Last() + ".txt";
-                // Write all deltas separated by newlines to file
-                // System.IO.File.WriteAllLines(outputFilePath, PcapsFromFolderToDeltas(directoryPath));
-                deltas.AddRange(PcapsFromFolderToDeltas(directoryPath));
-            });*/
-
+            // Go through all subdirectories & start a task for each to calculate deltas from .pcap files.
             directories.ForEach(directoryPath =>
             {
-                var outputFilePath = outputPath + directoryPath.Split('\\').Last() + ".txt";
+
                 tasks.Add(PcapsFromFolderToDeltasAsync(directoryPath));
             });
 
             while (tasks.Any())
             {
+                // Wait for a task to finish
                 Task<List<string>> finishedTask = await Task.WhenAny(tasks);
+                // Remove finished task from task list
                 tasks.Remove(finishedTask);
+                // Add result of finished task to list of deltas
                 deltas.AddRange( await finishedTask);
             }
 
-            Console.WriteLine(deltas.Count);
-            
+            // Output amount of deltas
+            Console.WriteLine(deltas.Count);            
 
+            // Split deltas into subsets of 10000
             var dataSets = deltas.Partition(10000);
             int i = 1;
             dataSets.ForEach(dataSet =>
             {
+                // Ensure that no incomplete set is written.
                 if (dataSet.Count == 10000)
                 {
                     System.IO.File.WriteAllLines(outputPath + $"\\{i}.txt", dataSet);
@@ -83,54 +87,24 @@ namespace PcapConverter
                 };                
             });
 
-            Console.WriteLine($"Invalid .pcap: {errors}");
+            Console.WriteLine($"Invalid .pcap files: {errors}");
             Console.WriteLine($"Written datasets: {i-1}");
-
-            //var counts = ValidatePcaps(inputPath);
-            //Console.WriteLine(counts);
+            Console.WriteLine($"Dropped deltas: {deltas.Count % 10000}");
         }
-                
-
-        public static List<Tuple<string, int, int>> ValidatePcaps(string path)
-        {
-            var result = new List<Tuple<string, int, int>>();            
-
-
-            var directories = Directory.GetDirectories(path).ToList();
-            directories.ForEach(directoryPath =>
-            {
-                var helloPackages = new List<Package>();
-                var clientKeyExchangePackages = new List<Package>();
-
-                Directory.GetFiles(directoryPath).ToList().ForEach(file =>
-                {
-                    List<Package> packageList = File.ReadAllLines(file)
-                                               .Select(v => Package.FromCsv(v))
-                                               .ToList();
-
-                    var startPackage = from package in packageList
-                                       where package.Info.Equals("TLSv1 375 Client Hello")
-                                       select package;
-                    var endPackage = from package in packageList
-                                     where package.Info.StartsWith("TLSv1.2 194 Client Key Exchange")
-                                     select package;
-
-                    helloPackages.AddRange(startPackage);
-                    clientKeyExchangePackages.AddRange(endPackage);
-                });
-
-                result.Add(new Tuple<string, int, int>(directoryPath ,helloPackages.Count, clientKeyExchangePackages.Count));
-            });
-
-            return result;
-        }
-
+             
+        /// <summary>
+        /// Calculate all time deltas from any amount of .pcap files in a specified folder
+        /// </summary>
+        /// <param name="folder">The path to the folder</param>
+        /// <returns>A List of strings containing the time deltas</returns>
         public static List<string> PcapsFromFolderToDeltas(string folder)
         {
             Console.WriteLine($"Current Folder: {folder}");
-            // Get all files in data directory and calculate time deltas
+            // Get all deltas using optional double. If a .pcap is erroneous save null.
             List<double?> deltas = new();
             Directory.GetFiles(folder, "*.csv").ToList().ForEach(f => deltas.Add(PcapToDelta(f)));
+
+            // Remove all null entries
             var timeDeltas = from delta in deltas
                                       where delta.HasValue
                                       select delta.Value.ToString();
@@ -138,19 +112,31 @@ namespace PcapConverter
             return timeDeltas.ToList();
         }
 
+        /// <summary>
+        /// Calculate all time deltas from any amount of .pcap files in a specified folder asynchronously
+        /// </summary>
+        /// <param name="folder">The path to the folder</param>
+        /// <returns>A Task to calculate a list of strings containing the time deltas</returns>
         public static async Task<List<string>> PcapsFromFolderToDeltasAsync(string folder)
         {
-            Console.WriteLine($"Current Folder: {folder}");
-            // Get all files in data directory and calculate time deltas
+            // Get all deltas using optional double. If a .pcap is erroneous save null.
             List<double?> deltas = new();
             await Task.Run(() => Directory.GetFiles(folder, "*.csv").ToList().ForEach(f => deltas.Add(PcapToDelta(f))));
+
+            // Remove all null entries
             var timeDeltas = from delta in deltas
                              where delta.HasValue
                              select delta.Value.ToString();
 
+            Console.WriteLine($"Finished Folder: {folder}");
             return timeDeltas.ToList();
         }
 
+        /// <summary>
+        /// Calculates the timedelta from a .pcap file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>A double if the .pcap is valid or null if it's malformed.</returns>
         public static double? PcapToDelta(string path)
         {
             double? res = null;
@@ -163,6 +149,7 @@ namespace PcapConverter
             IEnumerable<Package> startPackage;
             IEnumerable<Package> endPackage;
 
+            // depending on the version the info column slightly varies.
             if (isUnpatched)
             {
                 startPackage = from package in packageList
@@ -182,46 +169,82 @@ namespace PcapConverter
                                  select package;                
             }            
 
-            if (startPackage.Count() == 1 && endPackage.Count() == 1 && startPackage.First().Id == 4 && endPackage.First().Id == 8)
+            // Check if the pcap is malformed
+            if (startPackage.Count() == 1 && endPackage.Count() == 1 && startPackage.First().Index == 4 && endPackage.First().Index == 8)
             {
                 res = GetDelta(startPackage.Last(), endPackage.Last());
-            } else { errors++; }
+            } else 
+            { 
+                errors++; 
+            }
                         
             return res;
         }
-
+        /// <summary>
+        /// Calculate the elapsed time between 2 packages.
+        /// </summary>
+        /// <param name="startPackage"></param>
+        /// <param name="endPackage"></param>
+        /// <returns></returns>
         public static double GetDelta(Package startPackage, Package endPackage)
         {
             return endPackage.TimeDelta - startPackage.TimeDelta;
         }
     }
 
+    /// <summary>
+    /// The Package class holds all relevant information from a single package in a .pcap file created by Wireshark.
+    /// </summary>
     public class Package
-    {
-        public int Id { get; set; }
+    {        
+        public int Index { get; set; }
+        /// <summary>
+        /// Contains the elapsed time since the start of the package capturing session
+        /// </summary>
         public double TimeDelta { get; set; }
+        /// <summary>
+        /// This string corresponds to Wiresharks info column
+        /// </summary>
         public string Info { get; set; }
 
-        public Package(string id, string timeDelta, string info)
+        public Package(string index, string timeDelta, string info)
         {
-            Id = int.Parse(id);
-            TimeDelta = int.Parse(timeDelta.Split('.')[1]);
+            Index = int.Parse(index);
+            TimeDelta = double.Parse(timeDelta);
             Info = info;
         }
 
+        /// <summary>
+        /// Converts a line of text from a .csv created by Tshark into a Package
+        /// </summary>
+        /// <param name="csvLine">A line of .txt</param>
+        /// <returns>A new Package object</returns>
         public static Package FromCsv(string csvLine)
         {
+            /* Tshark produces extremely malformed .csv files that are not properly separated,
+             * so we need to replace the irregular ammount of spaces used to separate relevant 
+             * fields with tabs before we split the input. */
             string[] values = csvLine.TrimStart().Replace("    ", "\t").Replace("   ", "\t").Split('\t');
+            // Confirm that the splitting has produced a correctly sized array.
             if (values.Length == 4)
             {
-
+                // values[0] => Index | values[1] => TimeDelta | values[3] => Info | values[2] contains multiple irrelevant columns
                 return new(values[0], values[1], values[3]);
             }            
             return new("-1", "0.0", "Malformed Package");
         }
     }
+
+    
     public static class Extensions
     {
+        /// <summary>
+        /// Split a List <paramref name="values"/> into multiples lists with <paramref name="chunkSize"/> length.
+        /// </summary>
+        /// <typeparam name="T">Generic type of List</typeparam>
+        /// <param name="values">The List beeing split</param>
+        /// <param name="chunkSize">The size of the created lists</param>
+        /// <returns>A List of Lists of the specified size</returns>
         public static List<List<T>> Partition<T>(this List<T> values, int chunkSize)
         {
             return values.Select((x, i) => new { Index = i, Value = x })
